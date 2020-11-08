3e39d67aed00999f46162bc9971b491b5f189a21
JuYanYan <1429543751@qq.com>
2020-11-08 17:45:44 +0800

    初始提交 & 第一段代码

---
+++ README.md
@@ -0,0 +1,15 @@
+# ToytoyFuckC
+
+&emsp;&emsp;Toy, C Source Code, Playground.
+
+&emsp;&emsp;What? This is C Source code??????
+
+### File list
+
+| File      | Enviroment         | Content                    |
+| --------- | ------------------ | -------------------------- |
+| sort3.cpp | Visual Studio 2019 | Sort three number: a, b, c |
+
+&emsp;&emsp;文件里面就不管是什么语言了，因为好难排版的说。
+
+&emsp;&emsp;It doesn't matter what language it is in the document, because it's hard to typeset it.
---
+++ sort3.cpp
@@ -0,0 +1,18 @@
+﻿%:define _CRT_SECURE_NO_WARNINGS /* Copyright (C) JuYan, all rights reserved..
+!! 代码功能: 如下图/输入三个数 @ a b c !#! 按照从大到小的顺序输出 2020-11-08*/
+%:include <stdio.h> //________________________________________________________
+
+    int/*@@#                   JUYAN*/main                   (){const//@||
+   int    val     =/*         $$$         $$$  ###\         $$*/         sizeof
+ (int)     *8-      1/*       */;         int    a,b        ,c,          min,m\
+id,          max      ;;;    scanf       ("%d%"   "d%d"     ,&a          ,&b,\
+&c)         ;min        =(b   +((         a-b        )&((   a-b                                                      
+)>>          val         )))  ;;min=(c+(((min)        -(c)) &((
+min-c)>>val)));;        max   =(a-       ((a-        b)&(  (a-b)
+>>           val      )));    max          =((     max      -((
+max          -c)    &((       max         /**    ##//       **/           -c)
+>>(         (val  ))))        ));         mid  =a+          b+c          -min-
+max;        /*%              !WUST       juyan              @*/           pri\
+ntf       ("%d,"             "%d,%d\n",max,                  mid,min);;;;}
+
+%:define _stdio_h__ __________________________________________________________
